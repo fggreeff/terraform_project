499d1c03492dc42c4e2f24e510ee943fc68f2fa5
Francois Greeff <francois.greeff@bglgroup.co.uk>
2019-07-30 14:45:12 +0100

    Create public subnets

--- vcp_and_ec2/infrastructure/variables.tf
+++ vcp_and_ec2/infrastructure/variables.tf
@@ -7,3 +7,15 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
   description = "VPC CIDR block"
 }
+
+variable "public_subnet_1_cidr" {
+  description = "Public subnet 1 CIDR"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "Public subnet 2 CIDR"
+}
+
+variable "public_subnet_3_cidr" {
+  description = "Public subnet 3 CIDR"
+}
