30e7384f720b2e6cedab90baf183cf5ef2760c2d
Francois <fggreeff@gmail.com>
2019-10-09 15:08:52 +0100

    Setup layer 2 EC2 (#2)
    * Setup layer2 infrastructure. Config, variables & S3
    
    * Add pub & priv security group
    
    * Create IAM Role for EC2 instance
    
    * Create IAM Role Policy for EC2 instance
    
    * Create IAM Instance Profile
    
    * Add ability to dynamically get latest ami from amazon
    
    * Launch config for private EC2 Instances
    
    * Launch config for public EC2 Instances

---
+++ vcp_and_ec2/infrastructure/instances/instances.tf
@@ -0,0 +1,191 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+terraform {
+  backend "s3" {}
+}
+
+# read outputs from prev layer of infrastructure
+# read the remote state config, we make use of subnet ids, vpc cidr blocks
+data "terraform_remote_state" "network_configuration" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.remote_state_bucket}"
+    key    = "${var.remote_state_key}"
+    region = "${var.region}"
+  }
+}
+
+resource "aws_security_group" "ec2_public_security_group" {
+  name        = "EC2-Public-SG"
+  description = "Internet reaching access for EC2 Instances"
+  vpc_id      = "${data.terraform_remote_state.network_configuration.vpc_id}"
+
+
+  ingress {
+    from_port   = 80
+    protocol    = "TCP"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # ssh access
+  ingress {
+    from_port   = 22
+    protocol    = "TCP"
+    to_port     = 22
+    cidr_blocks = ["82.13.115.86/32"] #whatismyip
+  }
+  # allow to output traffic from EC2
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"] # allow output traffic to everywhere
+  }
+}
+
+resource "aws_security_group" "ec2_private_security_group" {
+  name        = "EC2-Private-SG"
+  description = "Only allow public SG resources to access these instances"
+  vpc_id      = "${data.terraform_remote_state.network_configuration.vpc_id}"
+
+  # allow ingress traffic from pub security group
+  ingress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["${aws_security_group.ec2_public_security_group.id}"]
+  }
+
+  ingress {
+    from_port   = 80
+    protocol    = "TCP"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow health checking for instances using this SG"
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"] # allow output traffic to everywhere
+  }
+}
+
+resource "aws_security_group" "elb_security_group" {
+  name        = "ELB-SG"
+  description = "ELB Security Group"
+  vpc_id      = "${data.terraform_remote_state.network_configuration.vpc_id}"
+
+  # internet facing loadbalancer
+  ingress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    description = "Allow wen traffic to load balancer"
+  }
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "ec2_iam_role" {
+  name               = "EC2-IAM-Role"
+  assume_role_policy = <<EOF
+{
+    "Version" : "2019-12-24",
+    "Statement": 
+    [
+        {
+            "Effect" : "Allow",
+            "Principal" : {
+                "Service" : ["ec2.amazonaws.com", "application-autoscaling.amazonaws.com"]
+            },
+            "Action" : "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ec2_iam_role_policy" {
+  name = "EC2-IAM-Policy"
+  role = "${aws_iam_role.ec2_iam_role.id}"
+  policy = <<EOF
+{
+    "Version" : "2019-12-24",
+        "Statement": 
+    [
+         {
+            "Effect" : "Allow",
+            "Action" : [
+                "ec2:*", 
+                "elasticloadbalancing:*",
+                "cloudwatch:*",
+                "logs:*"
+             ],
+             "Resource":"*" 
+         }
+    ]
+}
+EOF
+}
+
+# Instances that are launched with this instance profile, will have the above role & role policy
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "EC2-IAM-Instance-Profile"
+  role = "${aws_iam_role.ec2_iam_role.name}"
+}
+
+# Read latest ami from aws
+data "aws_ami" "launch_configuration_ami" {
+    most_recent = true
+    filter {
+        name   = "owner-alias"
+        values = ["amazon"]
+    }
+}
+
+resource "aws_launch_configuration" "ec2_private_launch_configuration" {
+    image_id                    = "${data.aws_ami.launch_configuration_ami.id}"
+    instance_type               = "${var.ec2_instance_type}"
+    key_name                    = "${var.key_pair_name}"
+    associate_public_ip_address = false
+    iam_instance_profile        = "${aws_iam_instance_profile.ec2_instance_profile.name}"
+    security_groups             = ["${aws_security_group.ec2_private_security_group.id}"]
+  
+  user_data = <<EOF
+    #!/bin/bash
+    yum update -y 
+    yum install httpd24 -y
+    service httpd start
+    chkconfig httpd on
+    export INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+    echo "<html><body><h1>Hello from Prod backend at intance <b>"$INSTANCE_ID"</b></h1></body></html>" > /var/www/html/index.html
+  EOF
+}
+
+resource "aws_launch_configuration" "ec2_public_launch_configuration" {
+    image_id = "${data.aws_ami.launch_configuration_ami.id}"
+    instance_type = "${var.ec2_instance_type}"
+    key_name = "${var.key_pair_name}"
+    associate_public_ip_address = true
+    iam_instance_profile = "${aws_iam_instance_profile.ec2_instance_profile.name}"
+    security_groups = ["${aws_security_group.ec2_public_security_group.id}"]
+
+  user_data = <<EOF
+    #!/bin/bash
+    yum update -y 
+    yum install httpd24 -y
+    service httpd start
+    chkconfig httpd on
+    export INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+    echo "<html><body><h1>Hello from Production Web App at intance <b>"$INSTANCE_ID"</b></h1></body></html>" > /var/www/html/index.html
+  EOF
+}
---
+++ vcp_and_ec2/infrastructure/instances/variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  default     = "eu-west-1"
+  description = "AWS Region"
+}
+
+variable "remote_state_bucket" {
+  description = "Bucket name for layer 1 remote state"
+}
+
+variable "remote_state_key" {
+  description = "Key name for layer 1 remote state"
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 Instance type to launch"
+}
+
+variable "key_pair_name" {
+  default     = "myEC2keypair"
+  description = "Keypair to use to connect to EC2 Instances"
+}
+
