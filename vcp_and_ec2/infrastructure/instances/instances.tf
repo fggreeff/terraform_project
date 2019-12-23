ddd6b1fb2e80fae51b6921581d87bd084b6b7fb7
Francois Greeff <francois.greeff@bglgroup.co.uk>
2019-12-23 09:54:36 +0000

    Fix security group issue

--- vcp_and_ec2/infrastructure/instances/instances.tf
+++ vcp_and_ec2/infrastructure/instances/instances.tf
@@ -56,7 +56,7 @@ resource "aws_security_group" "ec2_private_security_group" {
     from_port   = 0
     protocol    = "-1"
     to_port     = 0
-    cidr_blocks = ["${aws_security_group.ec2_public_security_group.id}"]
+    security_groups = ["${aws_security_group.ec2_public_security_group.id}"]
   }
 
   ingress {
